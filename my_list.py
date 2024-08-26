7fe32ac417b77fae5b3e8574d400c7276e8f29cc
GenioustaWiz <cm0215557@gmail.com>
2024-08-26 20:25:15 +0300

    first commit

---
+++ my_list.py
@@ -0,0 +1,24 @@
+my_list = [] 
+
+# Appending elements
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# Insert an element
+my_list.insert(1, 15)
+
+# Extending the list
+my_list.extend([50, 60, 70])
+
+# Removing the last element
+my_list.pop()
+
+# Sorting the list
+my_list.sort()
+
+# Finding the index of 30
+index = my_list.index(30)
+
+print("Index of 30:", index)
